1bb020a4b950c5455a430a8c1ad096cea1df8d58
KimJiTae <kimshinelove@gmail.com>
2016-02-13 12:47:41 +0900

    add getHeroesSlowly

--- app/hero.service.js
+++ app/hero.service.js
@@ -25,6 +25,13 @@ System.register(['angular2/core', './mock-heroes'], function(exports_1) {
                 HeroService.prototype.getHeroes = function () {
                     return Promise.resolve(mock_heroes_1.HEROES);
                 };
+                HeroService.prototype.getHeroesSlowly = function () {
+                    return new Promise(function (resolve) {
+                        return setTimeout(function () { return resolve(mock_heroes_1.HEROES); }, 2000);
+                    } // 2 seconds
+                     // 2 seconds
+                    );
+                };
                 HeroService = __decorate([
                     core_1.Injectable(), 
                     __metadata('design:paramtypes', [])
